e36075fc31574cda61c577aa3266cdb2e7cc4d84
Jackson Zou <jzou00@stuy.edu>
2019-02-04 10:52:52 -0500

    2/4: Changed helper method name to makeAllWordsH in Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -4,17 +4,17 @@ import java.util.List;
 public class Recursion{
   public static List<String> makeAllWords(int k, int maxLetter) {
     ArrayList<String> f = new ArrayList<>();
-    helper(k, "", maxLetter, f);
+    makeAllWordsH(k, "", maxLetter, f);
     return f;
   }
 
-  public static void helper(int k, String word, int maxLetter, List<String> data) {
+  public static void makeAllWordsH(int k, String word, int maxLetter, List<String> data) {
     if (k == 0) {
       data.add(word);
     }
     else {
       for (int i = 0; i < maxLetter; i++) {
-        helper(k-1,word+(char)('a'+i),maxLetter,data);
+        makeAllWordsH(k-1,word+(char)('a'+i),maxLetter,data);
       }
     }
   }
