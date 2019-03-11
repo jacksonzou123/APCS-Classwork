f76cec28caee2fef8f50045f5db969d9d9611ab4
Jackson Zou <jzou00@stuy.edu>
2019-03-11 11:00:21 -0400

    wroking partition

--- Partition.java
+++ Partition.java
@@ -11,14 +11,15 @@ public class Partition{
  */
   public static int partition(int[] ary, int start, int end) {
     Random random = new Random();
-    int rand = Math.abs(random.nextInt()) % (end - start + 1);
+    int rand = Math.abs(random.nextInt()) % (end - start - 1);
     int target = ary[rand];
     System.out.println("rand: " + rand + "target: "  + target);
     ary[rand] = ary[start];
     ary[start] = target;
     int begin = start;
     start++;
-    while (start != end) {
+    while (start < end) {
+
       if (target > ary[start]) {
         start++;
       }
@@ -43,9 +44,24 @@ public class Partition{
   }
 
   public static void main(String[] args) {
-    int[] ary = new int[] {5,3,6,7,4,1};
+    int[] ary = new int[] {5,3,6,7,4,1,2,10,1234,1232,13,34,23,122222,43};
     System.out.println(Arrays.toString(ary));
-    partition(ary, 0, 5);
+    int num = partition(ary, 0, ary.length-1);
+    System.out.println(num);
+    boolean f = true;
     System.out.println(Arrays.toString(ary));
+    for (int i = 0; i < ary.length; i++) {
+      if (i < num) {
+        if (ary[num] < ary[i]) {
+          f = false;
+        }
+      }
+      if (i > num) {
+        if (ary[num] > ary[i]) {
+          f = false;
+        }
+      }
+    }
+    System.out.println(f);
   }
 }
